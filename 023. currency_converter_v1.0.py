8e46291b0ca0847ff3f58c486b18b0c38b56dc9d
Jaanai-Lu <39607266+Jaanai-Lu@users.noreply.github.com>
2019-05-15 10:16:52 +0800

    Update 023. currency_converter_v1.0.py

--- 023. currency_converter_v1.0.py
+++ 023. currency_converter_v1.0.py
@@ -1,5 +1,17 @@
+"""
+   作者：陆震
+   功能：汇率兑换
+   版本：1.0
+   日期：15/05/2019
+"""
+
+# 汇率（常量不改变，大写放开头）
+USD_VS_RMB = 6.77
+# 人民币的输入
 rmb_str_value = input('请输入人民币（CNY)金额: ')
+# 将字符串转换为数字
 rmb_value = eval(rmb_str_value)
-usd_vs_rmb = 6.77
-usd_value = rmb_value / usd_vs_rmb
+# 汇率的计算
+usd_value = rmb_value / USD_VS_RMB
+# 输出结果
 print('美元（USD）金额是： ', usd_value)
