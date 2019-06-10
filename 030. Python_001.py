eb0d99e8c47c01b7fdb2752b7ecac04dc453b9f9
Jaanai_Lu <39607266+Jaanai-Lu@users.noreply.github.com>
2019-06-10 20:34:29 +0800

    Create 030. Python_001.py

---
+++ 030. Python_001.py
@@ -0,0 +1,52 @@
+"""
+    Python版本3.7.3
+    陆震
+    日期：10/06/2019
+"""
+
+
+# Python是一种易于学习又功能强大的编程语言。
+# 它提供了高效的高级数据结构，还有简单有效的面向对象编程。
+# Python优雅的语法和动态类型，以及解释型语言的本质，使它成为多数平台上写脚本和快速开发应用的理想语言。
+
+import this # Zen of Python(Python之禅)，列出Python设计的原则与哲学，有助于理解与使用这种语言
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
